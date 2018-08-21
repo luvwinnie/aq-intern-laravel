fc48ae4af8e8deebe0d051097a0bb147e47ab02b
Leow Chee Siang <cheesiangleow5284@gmail.com>
2018-08-21 17:23:24 +0900

    fix post_position bug without params

--- app/Http/Controllers/EmployeePositionController.php
+++ app/Http/Controllers/EmployeePositionController.php
@@ -41,20 +41,18 @@ class EmployeePositionController extends Controller
 
         $major = $request->input('major');
         $minor = $request->input('minor');
-//        return $given_name;
+
+        if ($major == "" || $minor == ""){
+            return response()->json(["error_message" => "error params"]);
+        }
 
         $position_id = Beacon::select('id','position')->where('major','like','%'.$major.'%')->where('minor','like','%'.$minor.'%')->get();
-//        dd($position_id);
+
         $id = $position_id[0]->id;
         $position = $position_id[0]->position;
-//        dd($position_id,$id,$position);
-//        dd($position_id[0]->id);
 
-//        $updated = DB::table('employees')->where('family_name','like','%'.$family_name.'%')->where('given_name','like','%'.$given_name.'%')->update(['beacon_id' => $id,'position' => $position]);
         $employees = Employee::select('employees.*')->where('family_name','like','%'.$family_name.'%')->where('given_name','like','%'.$given_name.'%')->update(['beacon_id' => $id,'position' => $position,'positioned_at' => Carbon::now()]);
-//        $employees->update(['beacon_id' => $id,'position' => $position]);
-//        echo $employees;
-        // $employees->update()
+
         if ($employees == 1){
             return response()->json(["message"=>"Update Successfully"]);
         }
