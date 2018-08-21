c9da4921437276c58671cca80d763995bb127178
Leow Chee Siang <cheesiangleow5284@gmail.com>
2018-08-21 15:21:49 +0900

    Added Beacon Post Request

--- app/Http/Controllers/EmployeePositionController.php
+++ app/Http/Controllers/EmployeePositionController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Employee;
+use App\Models\Beacon;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use App\Http\Controllers\DB;
 
 class EmployeePositionController extends Controller
 {
@@ -24,19 +27,43 @@ class EmployeePositionController extends Controller
      */
     public function get_position(Request $request,$family_name,$given_name)
     {
+
+        dd($family_name);
+
         $employees = Employee::select('employees.position')->where('family_name','like','%'.$family_name.'%')->where('given_name','like','%'.$given_name.'%');
-        // $employees = Employee::all/
-        if ($request->method() == "GET"){
-            
-            return response()->json($employees->get('position'));
-        }
         
+
+        return response()->json($employees->get('position'));
+
+
         
     }
 
     public function post_position(Request $request,$family_name,$given_name){
-        
-        return response()->json(["message"=>"post request"]);
+
+        $major = $request->input('major');
+        $minor = $request->input('minor');
+//        return $given_name;
+
+        $position_id = Beacon::select('id','position')->where('major','like','%'.$major.'%')->where('minor','like','%'.$minor.'%')->get();
+//        dd($position_id);
+        $id = $position_id[0]->id;
+        $position = $position_id[0]->position;
+//        dd($position_id,$id,$position);
+//        dd($position_id[0]->id);
+
+//        $updated = DB::table('employees')->where('family_name','like','%'.$family_name.'%')->where('given_name','like','%'.$given_name.'%')->update(['beacon_id' => $id,'position' => $position]);
+        $employees = Employee::select('employees.*')->where('family_name','like','%'.$family_name.'%')->where('given_name','like','%'.$given_name.'%')->update(['beacon_id' => $id,'position' => $position,'positioned_at' => Carbon::now()]);
+//        $employees->update(['beacon_id' => $id,'position' => $position]);
+//        echo $employees;
+        // $employees->update()
+        if ($employees == 1){
+            return response()->json(["message"=>"Update Successfully"]);
+        }
+
+        return response()->json(["message"=>"Update Failed "]);
+
+
         
     }
 
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,10 @@ class Employee extends Model
 {
     //
     protected $table= 'employees';
+
+    protected $fillable = [
+        "beacon_id",
+        "position",
+        "positioned_at",
+     ];
 }
